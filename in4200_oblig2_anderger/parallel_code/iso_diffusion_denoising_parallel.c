1f87fbd91920387c4845edd683f51f15dfd765d6
ageriksen <annikage@protonmail.com>
2024-04-30 21:59:24 +0200

    Parallel now compiles with mpicc and runs and produces a denoised image..ish
    there is a limited amount of result and there is a clear seam where the
    image has been stitched back.

--- in4200_oblig2_anderger/parallel_code/iso_diffusion_denoising_parallel.c
+++ in4200_oblig2_anderger/parallel_code/iso_diffusion_denoising_parallel.c
@@ -1,7 +1,66 @@
 #include "parallel_functions.h"
+#include <stdio.h>
+
+void exchange_halos(image *u, int my_rank, int num_procs)
+{
+    //halos above
+    if (my_rank > 0) {
+        MPI_Sendrecv(
+                u->image_data[0], u->n, MPI_FLOAT, my_rank-1, 0,
+                u->image_data[u->m - 1], u->n, MPI_FLOAT, my_rank-1, 0,
+                MPI_COMM_WORLD, MPI_STATUS_IGNORE
+                );
+    }
+    //halos below
+    if (my_rank < num_procs-1) {
+        MPI_Sendrecv(
+                u->image_data[u->m - 1], u->n, MPI_FLOAT, my_rank+1, 0,
+                u->image_data[0], u->n, MPI_FLOAT, my_rank+1, 0,
+                MPI_COMM_WORLD, MPI_STATUS_IGNORE
+                );
+    }
+}
 
 void iso_diffusion_denoising_parallel (image *u, image *u_bar, float kappa, int iters)
 {
 
-}
+    int my_rank, num_procs;
+    MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size (MPI_COMM_WORLD, &num_procs);
+    // Copy initial boundary conditions onto u_bar
+    for (int cols = 0; cols < (u->n); cols++) {
+        u_bar->image_data[0][cols] = u->image_data[0][cols]; // Copy top row
+        // printf("u[m-1=%d][%d] = %f\n", (u->m)-1, cols, u->image_data[(u->m)-1][cols]);
+        u_bar->image_data[(u->m) - 1][cols] = u->image_data[(u->m) - 1][cols]; // Copy bottom row
+    }
 
+    for (int rows = 0; rows < (u->m); rows++) {
+        u_bar->image_data[rows][0] = u->image_data[rows][0]; // Copy leftmost column
+        // printf("u[%d][n-1=%d] = %f\n", rows, (u->n)-1, u->image_data[rows][(u->n)-1]);
+        u_bar->image_data[rows][(u->n) - 1] = u->image_data[rows][(u->n) - 1]; // Copy rightmost column
+    }
+
+    image *temp;
+    for (int iteration=0; iteration<iters; iteration++) 
+    {
+        exchange_halos(u, my_rank, num_procs);
+        for (int i=1; i<(u->m)-1; i++) 
+        {
+            for (int j=1; j<(u->n)-1; j++) 
+            {
+                // printf("denoising loop i=%d, j=%d\n", i, j);
+                // printf("current image data: %f\n", u->image_data[i][j]);
+                u_bar->image_data[i][j] = u->image_data[i][j] + kappa*(
+                        (u->image_data[i-1][j]) + (u->image_data[i][j-1])
+                            -4.*(u->image_data[i][j])
+                        + (u->image_data[i][j+1]) + (u->image_data[i+1][j])
+                        );
+                // printf("denoised image data: %f\n", u_bar->image_data[i][j]);
+            }
+        }
+        temp = u_bar;
+        u_bar = u;
+        u = temp;
+    }
+    u_bar = u;
+}
--- in4200_oblig2_anderger/parallel_code/parallel_main.c
+++ in4200_oblig2_anderger/parallel_code/parallel_main.c
@@ -3,6 +3,14 @@
 /* other needed header files */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define min(a,b)             \
+({                           \
+    __typeof__ (a) _a = (a); \
+    __typeof__ (b) _b = (b); \
+    _a < _b ? _a : _b;       \
+})
 
 int main(int argc, char *argv[])
 {
@@ -42,7 +50,6 @@ int main(int argc, char *argv[])
     /* If there is a neighbor from below, allocate one more row; Same if there is neighbor from above */
     my_m = m/num_procs+2; // base size + 2 ghost rows
     if (my_rank<(m%num_procs)) {my_m++; } // add eventual remainder rows
-    int my_stride = my_m -2;
     if (my_rank == 0) { my_m--; } // if top or bottom, only 1 ghost, so remove 1 row
     if (my_rank == num_procs-1) { my_m--; }
     // my_m = ...;
@@ -52,14 +59,70 @@ int main(int argc, char *argv[])
 
     /* each process asks process 0 for a partitioned region */
     /* of image_chars and copy the values into u */
+    int stride = m/num_procs;
+    int remainder = m%num_procs;
+    int my_start = my_rank*stride;
+    int my_end = my_start + stride - 1;
+
+    if (my_rank < remainder) {
+        my_start += my_rank;
+        my_end += my_rank + 1;
+    }else {
+        my_start += remainder;
+        my_end += remainder;
+    }
+    my_end = min(my_end, m-1);
+
+    // mpi_comm calls
+    // my_image_chars = (unsigned char*)malloc((my_end - my_start + 1)*n*sizeof(unsigned char));
+    my_image_chars = (unsigned char*)malloc(my_m*n*sizeof(unsigned char));
+    MPI_Scatter(
+            image_chars, stride*n, MPI_UNSIGNED_CHAR,
+            my_image_chars, (my_end - my_start + 1)*n, MPI_UNSIGNED_CHAR,
+            0, MPI_COMM_WORLD
+            );
+    /*  ...  */
     /*  ...  */
 
     convert_jpeg_to_image (my_image_chars, &u);
     iso_diffusion_denoising_parallel (&u, &u_bar, kappa, iters);
-
+    
     /* each process sends its resulting content of u_bar to process 0 */
     /* process 0 receives from each process incoming values and */
     /* copy them into the designated region of struct whole_image */
+    MPI_Barrier(MPI_COMM_WORLD);
+    image my_u;
+    if (my_rank==0 || my_rank == num_procs-1) {
+        allocate_image(&my_u, my_m-1, n);
+    } else {
+        allocate_image(&my_u, my_m-2, n);
+    }
+    if (my_rank == 0) {
+        for (int i=0; i<my_u.m; i++) {
+            for (int j=0; j<my_u.n;j++) {
+                my_u.image_data[i][j] = u_bar.image_data[i][j];
+            }
+        }
+    }else if (my_rank == num_procs-1) {
+        for (int i=0; i<my_u.m; i++) {
+            for (int j=0; j<my_u.n;j++) {
+                my_u.image_data[i][j] = u_bar.image_data[i+1][j];
+            }
+        }
+    }else {
+        for (int i=0; i<my_u.m; i++) {
+            for (int j=0; j<my_u.n;j++) {
+                my_u.image_data[i][j] = u_bar.image_data[i+1][j];
+            }
+        }
+    }
+    int gather_count = (my_rank == 0 || my_rank == num_procs-1)?(my_m-1)*n:(my_m-2)*n;
+    
+    MPI_Gather(
+            my_u.image_data[0], gather_count, MPI_FLOAT, 
+            whole_image.image_data[0], gather_count, MPI_FLOAT, 0, 
+            MPI_COMM_WORLD
+            );
     /*  ...  */
 
     if (my_rank==0) {
